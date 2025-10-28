6e558616145f499034917e9ca6f86652c7b990b6
Gaste <gastineaulouis@gmail.com>
2025-10-28 12:16:19 +0100

    jpp

--- src/test/java/OrderManagementTest.java
+++ src/test/java/OrderManagementTest.java
@@ -1,46 +1,55 @@
 import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import fr.ekod.exceptions.InvalidDiscountCodeException;
-// import fr.ekod.exceptions.OutOfStockException;
-// import fr.ekod.Invoice;
-// import fr.ekod.Order;
-// import fr.ekod.Product;
-// import fr.ekod.ShoppingCart;
+import fr.ekod.exceptions.InvalidDiscountCodeException;
+import fr.ekod.exceptions.OutOfStockException;
+import fr.ekod.Invoice;
+import fr.ekod.Order;
+import fr.ekod.Product;
+import fr.ekod.ShoppingCart;
 
-// import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class OrderManagementTest {
-    // TODO: Declare products and cart
+
+    private Product testProduct;
+    private ShoppingCart testCart;
 
     @BeforeEach
     void setUp() {
-        // TODO: Initialize products and cart
+        testProduct = new Product("", 999.99, 5);
+        testCart = new ShoppingCart();
     }
 
-        @Test
+    @Test
     void testProductCreationNoName() {
-        Product testProduct = new Product("", 999.99, 5);
         assertEquals("", testProduct.getName());
         assertEquals(999.99, testProduct.getPrice());
         assertEquals(5, testProduct.getStockQuantity());
     }
+
     @Test
     void testProductCreationNoPrice() {
-        Product testProduct = new Product("Test Product", 0.0, 5);
-        assertEquals("Test Product", testProduct.getName());
-        assertEquals(0.0, testProduct.getPrice());
-        assertEquals(5, testProduct.getStockQuantity());
-    }
-    @Test
-    void testProductCreationNoStock() {
-        Product testProduct = new Product("Test Product", 999.99, 0);
-        assertEquals("Test Product", testProduct.getName());
-        assertEquals(999.99, testProduct.getPrice());
-        assertEquals(0, testProduct.getStockQuantity());
+        Product zeroPriceProduct = new Product("Test Product", 0.0, 5);
+        assertEquals("Test Product", zeroPriceProduct.getName());
+        assertEquals(0.0, zeroPriceProduct.getPrice());
+        assertEquals(5, zeroPriceProduct.getStockQuantity());
     }
 
-    // TODO: Implement tests for ShoppingCart
+    @Test
+    void testProductCreationNoStock() {
+        Product noStockProduct = new Product("Test Product", 999.99, 0);
+        assertEquals("Test Product", noStockProduct.getName());
+        assertEquals(999.99, noStockProduct.getPrice());
+        assertEquals(0, noStockProduct.getStockQuantity());
+    }
+
+    @Test
+    void testAddProductToCart() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addProduct(testProduct);
+        assertEquals(1, cart.getItemCount());
+        assertEquals(testProduct, pr());
+    }
 
     // TODO: Implement tests for Invoice
 
